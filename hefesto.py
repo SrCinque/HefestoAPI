66a90723c04735c9e12373e9242ae74c78a57768
SrCinque <filipe.cinque@gmail.com>
2023-10-16 13:06:53 -0300

    Estrutura da API 80%

--- hefesto.py
+++ hefesto.py
@@ -1,3 +1,72 @@
-import tensorflow as tf  # now import the tensorflow module
-print(tf.version)  # make sure the version is 2.x
-print('Hello word')
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+
+
+
+# Load dataset.
+dftrain = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vRhuroKnCWjAHU_BxSIi8Fca68wejgeNtv73LwMCocROjDW8kLC_E4gFQjrPvlqs08ylnouLU-tbSec/pub?output=csv') # training data
+y_train = dftrain.pop('seguro')
+
+#o dataset vai mudar quando eu fizer a rota de treinamento 
+
+
+CATEGORICAL_COLUMNS = ['latitude', 'longitude','nav_user','hour_login','sys_user','IP_user']
+NUMERIC_COLUMNS = []
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+  vocabulary = dftrain[feature_name].unique()  # gets a list of all unique values from given feature column
+  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+  feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+  def input_function():  # inner function, this will be returned
+    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
+    if shuffle:
+      ds = ds.shuffle(1000)  # randomize order of data
+    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
+    return ds  # return a batch of the dataset
+  return input_function  # return a function object for use
+
+
+
+
+train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
+
+
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+
+linear_est.train(train_input_fn)
+
+
+
+   
+
+def rest_trainTest(latitude,longitude,nav_user,hour_login,sys_user,user_ip):
+    data = {'latitude': latitude, 'longitude':longitude,'nav_user': nav_user,'hour_login': hour_login,'sys_user': sys_user,'IP_user': user_ip}
+    dfeval = pd.DataFrame(data, index=[0])
+    y_eval = pd.DataFrame({'seguro':'0'}, index=[0])
+    eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+    result = list(linear_est.predict(eval_input_fn))
+    resultado = result[0]['probabilities']
+
+    resposta = ''
+    if(resultado[0] < 0.50):
+       resposta = 'Não é seguro'
+    else:
+       resposta = 'Seguro'
+    return resposta
+
+      
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,133 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import hefesto
+import mysql.connector
+from mysql.connector import Error
+#imports para gerar um ID randomico
+import random
+import string
+from typing import Optional
+from pydantic import BaseModel
+import requests
 
-app = FastAPI()
+"""data = {
+    "latitude": 'Test',
+    "longitude": 'Test',
+    "nav_user": 'Test',
+    "hour_login": 'Test',
+    "sys_user": 'Test',
+    "IP_user": 'Test',
+    "seguro": 'Test'
+}
+
+def env_solit():
+    response = requests.post("http://127.0.0.1:8000/solicitacao/", data=data)
+    print(response.content)"""
+
+
+class dataAnltc(BaseModel):
+    #modelo/SCHEMA do Json para o DADOS
+    latitude: str
+    longitude: str
+    nav_user: int
+    hour_login: str
+    sys_user: int
+    IP_user: str
+    seguro: Optional[int] = None
+
+
+def insert_database(latitude,longitude,nav_user,hour_login,sys_user,user_ip,seguro):
+    #essa função insere no DB a linha de dados
+    connection = create_server_connection( )
+
+    cursor = connection.cursor()
+    #random_id = generate_random_id(10)
+    try:
+        comando_createtable = f"INSERT INTO data_hefestos (latitude, longitude, nav_user, hour_login, sys_user, user_ip, seguro) VALUES ('"+latitude+"','"+longitude+"','"+str(nav_user)+"','"+hour_login+"','"+str(sys_user)+"','"+user_ip+"','"+str(seguro)+"');"
+        print(comando_createtable)
+        cursor.execute(comando_createtable)
+        connection.commit()
+        resp = "Inserido no BD"
+    except Error as err:
+        print(f"Error: '{err}'")
+        resp = "Deu Bosta!"
+    return  resp
+
+
+
+def create_server_connection():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+           host='localhost',
+    user='root',
+    passwd='',
+    database='hefestapibd'
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+
+
+app = FastAPI()#Cria a API
+
+#Função para gerar um ID randomico 
+def generate_random_id(length):
+  """Gera um ID aleatório de um determinado comprimento."""
+  # Gera uma lista de caracteres aleatórios.
+  characters = list(string.ascii_letters + string.digits)
+  random.shuffle(characters)
+  # Seleciona os primeiros `length` caracteres da lista.
+  random_id = ''.join(characters[:length])
+  # Retorna o ID aleatório.
+  return random_id
 
 @app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+def home():
+    return{'message': 'Olá, essa é a URL principal'}
+
+@app.post("/insert/")
+#nessa solicitação eu passo os parametros que serão analisados
+async def criar_solicitacao(request: dataAnltc):
+    insert = insert_database(request.latitude, request.longitude, request.nav_user, request.hour_login, request.sys_user, request.IP_user, request.seguro)
+    if(insert == 'Inserido no BD'):
+        http = True
+    if(insert == 'Deu Bosta!'):
+        http = False
+    return{'message': http}    
+
+@app.post("/push")
+async def root(request: dataAnltc):
+    test = hefesto.rest_trainTest(request.latitude, request.longitude, request.nav_user, request.hour_login, request.sys_user, request.IP_user)
+    if(test == 'Não é seguro'):
+        esc = 'not'
+        seg = 1
+        print(esc)
+    if(test == 'Seguro'):
+        esc = 'yes'
+        seg = 0
+        print(esc)
+    insert = insert_database(request.latitude, request.longitude, request.nav_user, request.hour_login, request.sys_user, request.IP_user,seg)
+    print(insert)
+    return{"resposta": esc}
+
+
+@app.get("/mysql")
+#nessa função eu recebo o id da applicação que é o nome que darei na tabela   
+def cadastra_db():
+    connection = create_server_connection( )
+
+    cursor = connection.cursor()
+    random_id = generate_random_id(10)
+    try:
+        comando_createtable = f"CREATE TABLE `data_hefestos`(\nlatitude VARCHAR(100),\nlongitude VARCHAR(100),\nnav_user INT(6),\nhour_login VARCHAR(50),\nsys_user INT(6),\nuser_ip VARCHAR(100));"
+        print(comando_createtable)
+        cursor.execute(comando_createtable)
+        connection.commit()
+        resp = "Banco de dados criado!"
+    except Error as err:
+        print(f"Error: '{err}'")
+        resp = "Deu Bosta!"
+    return {'message': resp}
